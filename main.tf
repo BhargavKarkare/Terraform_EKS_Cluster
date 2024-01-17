5218626fce6bd8a6c29ac85a12627e83989d9dd1
Bhargav <karkarebhargav08@gmail.com>
2024-01-17 23:04:04 +0530

    modified main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = var.aws_region
 }
 
-module "eks_cluster" {
+module "eks" {
   source = "./eks_tf"
   aws_region = var.aws_region
   eks_cluster_name = var.eks_cluster_name
